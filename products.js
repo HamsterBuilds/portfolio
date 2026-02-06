6cf5378cf4e63d6ddd305fced77bca3c86c10c83
HamsterBuilds <estlincorporative@gmail.com>
2026-02-06 15:09:31 +0200

    Add files via upload

---
+++ products.js
@@ -0,0 +1,24 @@
+window.productsData = [
+    {
+        "name": "Christmas Pack",
+        "description": "Get this blocky 20+ christmas asset pack for FREE!",
+        "image": "https://i.imgur.com/iBi0ZUU.png",
+        "price": 0,
+        "detailedDescription": "",
+        "images": [
+            "data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' width='300' height='200' viewBox='0 0 300 200'%3E%3Crect fill='%23f8fafb' width='300' height='200'/%3E%3Ctext x='50%25' y='50%25' font-size='60' text-anchor='middle' dy='.3em'%3E🔜%3C/text%3E%3C/svg%3E"
+        ],
+        "purchaseLink": "https://hamsterbuilds.itch.io/christmas-pack"
+    },
+    {
+        "name": "Coming Soon!",
+        "description": "",
+        "image": "data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' width='300' height='200' viewBox='0 0 300 200'%3E%3Crect fill='%23f8fafb' width='300' height='200'/%3E%3Ctext x='50%25' y='50%25' font-size='60' text-anchor='middle' dy='.3em'%3E🔜%3C/text%3E%3C/svg%3E",
+        "price": 0,
+        "detailedDescription": "",
+        "images": [
+            "data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' width='300' height='200' viewBox='0 0 300 200'%3E%3Crect fill='%23f8fafb' width='300' height='200'/%3E%3Ctext x='50%25' y='50%25' font-size='60' text-anchor='middle' dy='.3em'%3E🔜%3C/text%3E%3C/svg%3E"
+        ],
+        "purchaseLink": ""
+    }
+];
---
+++ projects.js
@@ -0,0 +1,79 @@
+window.projectsData = [
+    {
+        "title": "Guns",
+        "description": "Ever needed a gun? I got you.",
+        "thumbnail": "https://i.imgur.com/ZYc1DmZ.png",
+        "type": "commission",
+        "style": "blocky",
+        "price": "600 robux",
+        "images": [
+            "https://i.imgur.com/ZYc1DmZ.png"
+        ]
+    },
+    {
+        "title": "Fast food",
+        "description": "Perfect for your restaurant!",
+        "thumbnail": "https://i.imgur.com/zIetexu.png",
+        "type": "commission",
+        "style": "blocky",
+        "price": "1000 robux",
+        "images": [
+            "https://i.imgur.com/zIetexu.png"
+        ]
+    },
+    {
+        "title": "Construction props",
+        "description": "Every builders dream",
+        "thumbnail": "https://i.imgur.com/IWgjxhN.png",
+        "type": "project",
+        "style": "blocky",
+        "price": "1000 robux",
+        "images": [
+            "https://i.imgur.com/IWgjxhN.png"
+        ]
+    },
+    {
+        "title": "2025 new year pack",
+        "description": "New year, new props!",
+        "thumbnail": "https://i.imgur.com/sGnEyhR.png",
+        "type": "project",
+        "style": "blocky",
+        "price": "1000 robux",
+        "images": [
+            "https://i.imgur.com/sGnEyhR.png"
+        ]
+    },
+    {
+        "title": "Flowers for everyone!",
+        "description": "I mean, who doesn't like flowers?",
+        "thumbnail": "https://i.imgur.com/9eiCU9x.png",
+        "type": "project",
+        "style": "blocky",
+        "price": "1000 robux",
+        "images": [
+            "https://i.imgur.com/9eiCU9x.png"
+        ]
+    },
+    {
+        "title": "Warehouse with a tank",
+        "description": "Why did the tank come through the wall?",
+        "thumbnail": "https://i.imgur.com/vEsieKI.png",
+        "type": "project",
+        "style": "studs",
+        "price": "300 robux",
+        "images": [
+            "https://i.imgur.com/vEsieKI.png"
+        ]
+    },
+    {
+        "title": "2 cars and a house",
+        "description": "Even the stop sign is happy!",
+        "thumbnail": "https://i.imgur.com/ApEtsgk.png",
+        "type": "project",
+        "style": "studs",
+        "price": "1000 robux",
+        "images": [
+            "https://i.imgur.com/ApEtsgk.png"
+        ]
+    }
+];
